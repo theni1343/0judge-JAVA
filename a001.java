ea79d43086c87b6706e41f347f5197b6ff127214
theni1343 <47775446+theni1343@users.noreply.github.com>
2019-07-09 11:22:30 +0800

    Create a001.java

---
+++ a001.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class JAVA
+{
+	public static void main(String[] args) {
+		
+		
+		Scanner a = new Scanner(System.in);
+		
+		
+		while(a.hasNext() ) {
+			
+			System.out.println("hello, " + a.nextLine());
+		}
+	}
+}
